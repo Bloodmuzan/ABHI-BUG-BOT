f1a88aa0229264c8e1ab46028f90477dbe06a81a
Bloodmuzan <142229100+Bloodmuzan@users.noreply.github.com>
2024-06-17 14:00:19 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,26 +2,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919074692450"
-global.ownername = "Abhishek Suresh"
+global.ownernomer = "22898133388"
+global.ownername = "𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛"
 global.ytname = "YT: Comedy Melody CH"
 global.socialm = "GitHub: DGXeon"
 global.location = "India, Kerala, Idukki"
 
-global.ownernumber = '919074692450'  //creator number
-global.ownername = 'Abhishek Suresh' //owner name
-global.botname = '𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
+global.ownernumber = '22898133388'  //creator number
+global.ownername = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛' //owner name
+global.botname = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
 //sticker details
-global.packname = '🎯𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃'
-global.author = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺 𝚂𝚄𝚁𝙴𝚂𝙷🍀'
+global.packname = '🩵𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛'
+global.author = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛🍀'
 
 //console view/theme
-global.themeemoji = '🎯'
-global.wm = "©𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃"
+global.themeemoji = '🩵'
+global.wm = "©𝙹𝚎𝚊𝚗-𝙿𝚊𝚛𝚔𝚎𝚛-𝙱𝙾𝚃"
 
 //theme link
-global.link = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
+global.link = 'https://chat.whatsapp.com/IAto4swnSi78hTHy9Rin61'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
@@ -33,7 +33,7 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
@@ -44,15 +44,15 @@ global.typemenu = 'v2'
 
 //reply messages
 global.mess = {
-    done: 'Done✅',
+    done: 'Done🩵',
     prem: 'This feature can be used by premium user only',
     admin: 'This feature can be used by admin only',
     botAdmin: 'This feature can only be used when the bot is a group admin ',
     owner: 'This feature can be used by owner only',
     group: 'This feature is only for groups',
     private: 'This feature is only for private chats',
-    wait: 'Please Wait',    
-    error: 'Error❌',
+    wait: 'Please Wait for parker',    
+    error: 'Error💔',
 }
 
 global.thumb = fs.readFileSync('./Media/Abhi.jpg')
